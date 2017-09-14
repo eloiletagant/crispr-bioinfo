f432c1dcb6416e5a335ad42c4e480c147e56ca47
Anatole <anatole.gilet@ntymail.com>
2017-09-14 15:49:42 +0200

    ajoute chargement et up seq placeholder

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include 'inc/header.php';
+//reset $session res to avoid get last query res
 $_SESSION['res'] = 'NULL';
 ?>
 
@@ -38,7 +39,7 @@ $_SESSION['res'] = 'NULL';
                       </div>
 
                      <div class="input-field col s12">
-                       <textarea id="seq" name="seq" class="materialize-textarea" placeholder="atgccgcgcgtcgtgcccgaccagagaagcaagttcgagaacgaggagttttttaggaagctgagccgcgagtgtgagattaagtacacgggcttcagggaccggccccacgaggaacgccaggcacgcttccagaacgcctgccgcgacggccgctcggaaatcgcttttgtggccacagaaccaatctgtctctccagttttttccggccagctggcagggagaacagcgacaaacacctagccgagagtatgtcgacttagaaagagaagcaggcaaggtatatttgaaggctcccatgattctgaatggagtctgtgttatctggaaaggctggattgatctccaaagactggatggtatgggctgtctggagtttgatgaggagcgagcccagcaggaggatgcattagcacaacaggcctttgaagaggctcggagaaggacacgcgaatttgaagatagagacaggtctcatcgggaggaaatggaggcaagaagacaacaagaccctagtcctggttccaatttaggtggtggtgatgacctcaaacttcgttaa"></textarea>
+                       <textarea id="seq" name="seq" class="materialize-textarea" placeholder="ATGGGTGATAAACTTCGATTATCCATCGGAATTTTGGGAAACGGAGCTTCTCTGTTGCTATATACAGCTCCAATAGTAACATTTTCAAGGGTGTTTAAGAAGAAAAGCACAGAGGAATTCTCATGTTTTCCTTACGTTATGACACTCTTTAACTGTTTGATATATACTTGGTACGGTTTACCGATTGTGAGTCATCTTTGGGAGAATCTTCCTCTCGTCACCATTAATGGAGTCGGCATCCTTCTCGAATCTATCTTCATTTTCATATATTTCTACTACGCATCACCAAAAGAAAAGATTAAGGTTGGTGTTACATTTGTTCCGGTGATCGTTGGGTTCGGCTTAACGACAGCAATCTCAGCCTTGGTATTTGACGACCATCGTCACCGAAAATCATTCGTCGGAAGTGTTGGCCTCGTGGCTTCCATCTCTATGTATGGTTCTCCTCTCGTCGTTATGAAGAAAGTGATAGAGACAAGAAGTGTGGAATACATGCCGTTTTACTTGTCCTTCTTCTCATTTCTGGCTAGTTCCCTTTGGTTGGCATATGGCTTACTCAGCCATGATCTCTTTCTTGCGTCACCTAATATGGTTGCGACTCCATTGGGAATTCTCCAACTTATCCTCTACTTCAAGTACAAGAATAAGAAGGATTTAGCACCAACAACAATGGTGATCACCAAACGAAATGATCATGATGACAAGAACAAAGCCACACTTGAGTTTGTTGTTGACGTTGATCGTAATAGTGATACCAATGAGAAGAATTCTAACAATGCCTCATCGATCTAA"></textarea>
                        <label for="seq">Paste a nucleotide sequence</label>
                      </div>
                  </div>
@@ -51,16 +52,7 @@ $_SESSION['res'] = 'NULL';
       </form>
 
     </div>
-<!--
-    <div>
-        Sed nulla cred Banksy jean shorts. Reprehenderit mumblecore incididunt anim accusamus. Keffiyeh Cosby sweater in cornhole elit, tote bag cillum banjo. Shabby chic YOLO banh mi sunt. Artisan blog Neutra, polaroid adipisicing Banksy +1 lo-fi umami distillery fixie seitan. Semiotics artisan flannel mollit craft beer Blue Bottle. Bespoke biodiesel banh mi literally.
 
-YOLO Marfa drinking vinegar polaroid laborum veniam, est disrupt umami wayfarers try-hard before they sold out fap. Culpa bitters kitsch adipisicing, nesciunt brunch squid minim post-ironic assumenda irony. Qui bespoke organic, photo booth authentic PBR velit before they sold out fingerstache nostrud flannel. Leggings chillwave est sustainable, tofu raw denim sint 8-bit keffiyeh High Life. Nostrud Austin put a bird on it, typewriter Godard gastropub ennui mlkshk deserunt assumenda. Blog letterpress meggings, nihil Shoreditch Etsy try-hard vinyl Echo Park cray farm-to-table McSweeney's High Life. Qui stumptown ex Cosby sweater street art ethnic irure dolore.
-
-In post-ironic umami, sint +1 gluten-free pickled disrupt vinyl jean shorts velit placeat est American Apparel. Flannel cupidatat deserunt master cleanse cornhole vinyl, Banksy 8-bit fugiat pariatur. Schlitz Intelligentsia incididunt ugh, literally minim skateboard ennui. Labore deep v placeat, mlkshk biodiesel church-key post-ironic Cosby sweater twee nesciunt Blue Bottle. Salvia vero esse, exercitation cliche chambray aliquip gluten-free yr post-ironic pug narwhal kale chips ennui hella. Consequat bitters salvia delectus esse. Fap kogi forage culpa Tumblr, accusamus PBR Thundercats PBR&B banjo ullamco 8-bit bitters minim you probably haven't heard of them.
-
-Sed nulla cred Banksy jean shorts. Reprehenderit mumblecore incididunt anim accusamus. Keffiyeh Cosby sweater in cornhole elit, tote bag cillum banjo. Shabby chic YOLO banh mi sunt. Artisan blog Neutra, polaroid adipisicing Banksy +1 lo-fi umami distillery fixie seitan. Semiotics artisan flannel mollit craft beer Blue Bottle. Bespoke biodiesel banh mi literally.
-    </div>-->
 
 <script>
     $('#submit_seq').click(function() {
--- test1.php
+++ test1.php
@@ -1,12 +1,59 @@
-<?php include 'inc/header.php';
+<?php
+include 'inc/header.php';
 $data = $_SESSION['res'];
 ?>
 
 <div class="container">
     <br /><br /><br />
+
+<div class="center-align">
+    <div id="loader" class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-blue">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+
+      <div class="spinner-layer spinner-red">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+
+      <div class="spinner-layer spinner-yellow">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+
+      <div class="spinner-layer spinner-green">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div><div class="gap-patch">
+          <div class="circle"></div>
+        </div><div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
+</div>
+
+
     <div id="content">
     </div>
-    <table id="jsonTable">
+    <table id="jsonTable" style="height:300px;">
     </table>
 </div>
 
@@ -17,8 +64,22 @@ $(document).ready(function () {
     //"{"error":"","pam_sequence":"NGG","program_name":"CRISPRdirect","results":[],"sequence_name":"","specificity_check":"Human (Homo sapiens) genome, GRCh37/hg19 (Feb, 2009)","time":"2017-09-12 00:58:22"}"
 
     var data = '<?= $data ?>'
-    //console.log(data)
-    var json = JSON.parse(data);
+    console.log(data)
+    let done = false
+    while (!done) {
+        try {
+            // try to parse the response string
+            var json = JSON.parse(data);
+            done = true
+            $('#loader').css('display', 'none')
+        } catch (e) {
+            $('#loader').css('display', 'block')
+        }
+    }
+    console.log(json)
+
+
+
     console.log(json)
     if (json.error !== "") {
         $('#content').text("Une erreur est survenue : " + json.error)
