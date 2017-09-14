287b740458fa25121dca1ae00fc1fd90481deb91
Alexis A <alexis.amiand@gmail.com>
2017-09-14 12:08:14 +0200

    add des trucs

--- blast/script.js
+++ blast/script.js
@@ -1,15 +1,26 @@
+var interval;
+var seq;
+var seq.lenght;
+
+// MANIPULATION DE SEQUENCES
+
+
+
+// BLAST
 function testBlast(){
 	/*test function*/
 	var seq = document.getElementById("input-seq"); //user sequence
 	console.log(seq.value);
 	// var db = document.getElementById("input-organism");
 	var db = "nr";
-	var Tblast = blast(seq.value, db);
-
-	console.log("Result that we are looking for : ", Tblast);
+	var x = blast(seq.value, db, function(resultJSON){
+		console.log("nn", resultJSON);
+		return (resultJSON);
+	})
+	// var Tblast = blast(seq.value, db);
 }
 
-function blast(seq, db) {
+function blast(seq, db, callback) {
 	//BLaster execute un Blastn grace a une sequence/un locus ID en parametre et retourne un JSON des résultats
 	//@param seq needs to be only composed by "ATCG" (checked earlier)
 	//@param db id of the database
@@ -24,58 +35,77 @@ function blast(seq, db) {
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhr.send(data);
 
+
+
 	//Listenning to the XHR then get the RID from the xhr response.
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+	xhr.onload = function() {
 			//GET RID DU POST
-			var response = (xhr.responseText);
-			var parser = new DOMParser();
+			const parser = new DOMParser();
+			var response = xhr.responseText;
 			var htmlDoc = parser.parseFromString(response, "text/xml");
 			var ridObject = htmlDoc.getElementsByName("RID");
 			rid = ridObject[0].defaultValue;
 			console.log("RID :", rid);
-		}
-		//Get the results of the blast thanks to th RID
-		if (rid !== undefined) {
+			loopGetJSON(rid,
+				function(resultJSON){
+					if (resultJSON!==undefined){
+						console.log("resultJSON", resultJSON);
+						callback(resultJSON);
 
-			//Asking NCBI server for the blast results every 10 seconds
-			var interval = setInterval (function() {
-				//asking for the blast results
-				r = getJSONResults(rid);
-				console.log("R : " + r);
-				//if we have results, stop the loop then retrun the JSON
-				if (r !== undefined){
-					console.log("End of the loop");
-					clearInterval(interval); //stop the loop
-					return r; //return the json string to the parent function (which is interval)
+						//trie
+						//ajout au 
+					}
 				}
-			}, 10000)
-
-		} else {
-			console.log("RID is undefined");
+				);
 		}
-		return interval; //return the JSON string to the parent function (line 31 :/)
 	}
-}
 
-function getJSONResults(rid) {
+
+
+
+	function loopGetJSON(rid, callback){
+/*	Loop getJSON until a result is found
+@param rid request id*/
+var i = 0;
+			//Asking NCBI server for the blast results every 10 seconds
+			interval = setInterval (function() {
+				i ++;	
+				console.log(i)
+				//asking for the blast results
+				callback(getJSON(rid, function(resultJSON){
+					if (resultJSON!==undefined){
+						return resultJSON;
+					}
+				}))
+			},10000)
+		}
+
+
+		function getJSON(rid, callback) {
 /*	This function ask NCBI server for a BLAST result;
 	If the response is not "parsable", it means that NCBI servers don't have the blast resuls yet so we have to wait
-	@param rid the request id */
+	@param rid the request id 
+	@param cb th callback
+	*/
 	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://blast.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&FORMAT_TYPE=JSON2_S&RID=" + rid, true);
+	const url = "https://blast.ncbi.nlm.nih.gov/blast/Blast.cgi?CMD=Get&FORMAT_TYPE=JSON2_S&RID=" + rid;
+	xhr.open("GET", url, true);
+	console.log("url", url);
 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-			try {
+	xhr.send();
+	xhr.onload = function() {
+
+		try {
 				// try to parse the response string
 				var resultJSON = JSON.parse(xhr.responseText);
-				console.log("NCBI servers answered with the correct format, we now need to return these results");
-				return resultJSON;
+				clearInterval(interval);
+				callback(resultJSON);
 			} catch (e) {
 				console.log("WAITING");
 			}
 		}
 	}
-}
+
+
+
+// CRISPERPLANT
