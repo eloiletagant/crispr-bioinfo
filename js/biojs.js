059a865bb3849dc5655f1723379266bda6531cf3
Alexis A <alexis.amiand@gmail.com>
2017-09-14 19:07:46 +0200

    create buildplasmid function

--- js/biojs.js
+++ js/biojs.js
@@ -140,61 +140,57 @@ function filter(seq, crisprOuput){
 	var TMmin = TMexpect - TMerror //minimum du tm
 	var TMmax = TMexpect+ TMerror // maximum tu tm
 
-try {
 	crisprOuput = JSON.parse(crisprOuput); //recuperation des resultats
-} catch (e) {
-	console.log("cetait du json")
-}
-
-	var rows = crisprOuput //seq brute
+	var rows = crisprOuput.results //seq brute
+	var rowsFiltred = [];
 	var passe2 = false;
 	var passe3 = false;
-	var rowsFiltred = []
 
 
 	console.log("JSON pas filtré", rows)
 
 	for ( var i in rows){//PREMIERE PASSE - 20 nt
-	  // console.log("bite", rows[i]);
-	  // console.log("end", rows[i].end);
-	  // console.log("hit_20mer", rows[i].hit_20mer);
-	  // console.log("tm", rows[i].tm);
-	  if (rows[i].end <= posHalf && //ôsition
-	    (rows[i].hit_20mer <= nbrMaxTarget20 && rows[i].hit_20mer != 0) &&   //nombr de target pour 20
-	    (rows[i].tm>= TMmin && rows[i].tm <= TMmax))  //temperature
-	    {
-	      console.log("1row kept", rows[i])//filtres respectés on keep
-	      rowsFiltred.push(rows[i]);
-	    }
-	  }
+		// console.log("bite", rows[i]);
+		// console.log("end", rows[i].end);
+		// console.log("hit_20mer", rows[i].hit_20mer);
+		// console.log("tm", rows[i].tm);
+		if (rows[i].end <= posHalf && //ôsition
+			(rows[i].hit_20mer <= nbrMaxTarget20 && rows[i].hit_20mer != 0) &&   //nombr de target pour 20
+			(rows[i].tm>= TMmin && rows[i].tm <= TMmax))  //temperature
+			{
+				console.log("1row kept", rows[i])//filtres respectés on keep
+				rowsFiltred.push(rows[i]);
+			}
+		}
 
-	  console.log("leght passe 1",Object.keys(rowsFiltred).length);
-	  if (Object.keys(rowsFiltred).length == 0){ //Apres une premere loop on check si on a des results,
-	    console.log("deuxieme passe");
-	    passe2 = true;
-	    for (var i in rows){//DEUXIEME PASSE 12NT
+		console.log("leght passe 1",Object.keys(rowsFiltred).length);
+		if (Object.keys(rowsFiltred).length == 0){ //Apres une premere loop on check si on a des results,
+			console.log("deuxieme passe");
+			passe2 = true;
+			for (var i in rows){//DEUXIEME PASSE 12NT
 
-	      if (rows[i].end <= posHalf && //ôsition
-	        (rows[i].hit_12mer <= nbrMaxTarget12 && rows[i].hit_12mer != 0) &&
-	        (rows[i].tm>= TMmin && rows[i].tm <= TMmax))  //temperature
-	        {	console.log("2row kept", rows[i])
-	        rowsFiltred.push(rows[i])
-	      }
-	    }
-	  }
+				if (rows[i].end <= posHalf && //ôsition
+					(rows[i].hit_12mer <= nbrMaxTarget12 && rows[i].hit_12mer != 0) &&
+					(rows[i].tm>= TMmin && rows[i].tm <= TMmax))  //temperature
+					{	console.log("2row kept", rows[i])
+					rowsFiltred.push(rows[i])
+				}
+			}
+		}
 
-	  if (Object.keys(rowsFiltred).length == 0){//Apres une dexuime loop on check si on a des results,
-	    passe3=true;
-	    console.log("troisieme passe");
-	    for (var i in rows){//DEUXIEME PASSE 8NT
-	      if (rows[i].end <= posHalf &&
-	        (rows[i].hit_8mer <= nbrMaxTarget8 && rows[i].hit_8mer != 0) &&
-	        (rows[i].tm>= TMmin && rows[i].tm <= TMmax))
-	        {	console.log("3row kept", rows[i])
-	        rowsFiltred.push(rows[i])
-	      }
-	    }
-	  }
+		if (Object.keys(rowsFiltred).length == 0){//Apres une dexuime loop on check si on a des results,
+			passe3=true;
+			console.log("troisieme passe");
+			for (var i in rows){//DEUXIEME PASSE 8NT
+				if (rows[i].end <= posHalf &&
+					(rows[i].hit_8mer <= nbrMaxTarget8 && rows[i].hit_8mer != 0) &&
+					(rows[i].tm>= TMmin && rows[i].tm <= TMmax))
+					{	console.log("3row kept", rows[i])
+					rowsFiltred.push(rows[i])
+				}
+			}
+		}
+
 
 		console.log("JSON filtré", rowsFiltred) //affichage du json filtré
 		return rowsFiltred;
@@ -216,3 +212,30 @@ try {
 
 		return fullSeq;
 	}
+
+
+	function buildPlasmid(filteredJSON){
+		// a partir d'un json filtré de sgrna
+		// créer pour chaque resultat,  le brin + et le brin -
+		// les ajoute au json et renvoi le tout
+		// @param filterdJson le tableau detruncs filtré
+		// @return finalJsonl eJson avec les brins + et -
+		var seq;
+		var v;
+		var finalJSON = filteredJSON
+		for ( i  in finalJSON){
+			seq = filnalSON[i].sequence
+			seqStrand = finalJSON[i].strand
+			console.log("seq",seq)
+			console.log("strand",seqStrand)
+
+			if (seqStrand>0){//si le brun qu'on a est le plus
+				v = addExtremities(seq, seq.split("").reverse().join(""))
+			}
+		else {//sile brin est le -
+			v = addExtremities(seq.split("").reverse().join(""), seq)
+		}
+		finalJSON[i].sequence = v[0] +"<br>"+v[1]
+	}
+	return finalJSON
+}
