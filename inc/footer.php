f20ff9f457e35c6a2523251f2e7fdfc4139f530b
Alexis A <alexis.amiand@gmail.com>
2017-09-15 14:22:10 +0200

    footer

--- inc/footer.php
+++ inc/footer.php
@@ -6,7 +6,7 @@
         <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any advice could help and is so greatly appreciated.</p>
       </div>
 
-      <div class="col offset-l3 l3 s12">
+      <!-- <div class="col offset-l3 l3 s12">
         <h5 class="white-text">Connect</h5>
         <ul>
           <li><a class="white-text" href="#!">Link 1</a></li>
@@ -14,12 +14,14 @@
           <li><a class="white-text" href="#!">Link 3</a></li>
           <li><a class="white-text" href="#!">Link 4</a></li>
         </ul>
-      </div>
+      </div> -->
     </div>
   </div>
   <div class="footer-copyright">
     <div class="container">
-    Made by <a class="orange-text text-lighten-3" href="http://geniephy.net">GPhy</a> - BV Team
+    Made by <a class="orange-text text-lighten-3" href="http://geniephy.net">GPhy</a> - BV Team      |
+  <span style="color: #E3DCCD;">This page is licensed under a Creative Commons Attribution 4.0 International License (CC BY 4.0).</span>  
+
     </div>
   </div>
 </footer>
